cf7499ce560c2b3b28e7cf94526d0b71cb6c2d71
Tira M <146383949+tmssni@users.noreply.github.com>
2024-12-10 09:17:57 -0500

    Player movement

--- ghoul-duel/player.gd
+++ ghoul-duel/player.gd
@@ -7,7 +7,7 @@ var screen_size
 
 func _ready():
 	screen_size = get_viewport_rect().size
-	#hide()
+	hide()
 	
 func _process(delta):
 	var velocity = Vector2.ZERO
@@ -19,7 +19,13 @@ func _process(delta):
 		velocity.y += 1  
 	if Input.is_action_pressed("move_down"):
 		velocity.y -= 1 
-
+	
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite2D.play()
+	else:
+		$AnimatedSprite2D.stop()
+	
 	position += velocity * delta
 	position = position.clamp(Vector2.ZERO, screen_size)
 	
@@ -28,17 +34,20 @@ func _process(delta):
 	elif velocity.x != 0:
 		$AnimatedSprite2D.animation = "move"
 		$AnimatedSprite2D.flip_v = false
+		$Trail.rotation = 0
 		$AnimatedSprite2D.flip_h = velocity.x < 0
 	elif velocity.y != 0:
-		$AnimatedSprite2D.animation = "up"
-		$AnimatedSprite2D.flip_v = velocity.y > 0
+		$AnimatedSprite2D.animation = "idle"
+		rotation = PI if velocity.y > 0 else 0
+
+func start(pos):
+	position = pos
+	rotation = 0
+	show()
+	$CollisionShape2D.disabled = false
 
 func _on_body_entered(body):
 	hide() #player disappears after being hit
 	hit.emit()
 	$CollisionShape2D.set_deferred("disabled", true)
 	
-func start(pos):
-	position = pos
-	show()
-	$CollisionShape2D.disabled = false
