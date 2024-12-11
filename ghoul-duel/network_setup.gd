0de7345c8c1c2ed5805fc2150b62d776bbba7453
Tira M <146383949+tmssni@users.noreply.github.com>
2024-12-11 14:46:51 -0500

    Added network.gd

---
+++ ghoul-duel/network_setup.gd
@@ -0,0 +1,20 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+
+func _on_create_server_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_join_server_pressed() -> void:
+	pass # Replace with function body.
