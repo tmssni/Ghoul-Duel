8c0972102248ee55238cac775c7fefac0429e5ff
Tira M <massoni.tira@gmail.com>
2024-12-11 10:02:46 -0500

    Player and Enemy update

--- ghoul-duel/enemy.gd
+++ ghoul-duel/enemy.gd
@@ -1,25 +1,41 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = 0
+@export var speed = 400
+var screen_size
 
+func _ready():
+	screen_size = get_viewport_rect().size
+	#hide()
 
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+func _process(delta):
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1 
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1  
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1 
+	
+	velocity = velocity.normalized() * speed
+	$AnimatedSprite2D.play()
+	
+	position += velocity * delta
+	position = position.clamp(Vector2(), screen_size)
+	
+	if (velocity.x == 0 and velocity.y == 0) or velocity.y != 0:
+		$AnimatedSprite2D.animation = "idle"
+	elif velocity.x != 0:
+		$AnimatedSprite2D.animation = "move"
+		$AnimatedSprite2D.flip_v = false
+		#$Trail.rotation = 0
+		$AnimatedSprite2D.flip_h = velocity.x < 0
+	
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
-	move_and_slide()
+func start(pos):
+	position = pos
+	rotation = 0
+	show()
+	$CollisionShape2D.disabled = false
--- ghoul-duel/player.gd
+++ ghoul-duel/player.gd
@@ -1,14 +1,13 @@
-extends Area2D
+extends CharacterBody2D
 
-signal hit
 
 @export var speed = 400
-var screen_size 
+var screen_size
 
 func _ready():
 	screen_size = get_viewport_rect().size
 	#hide()
-	
+
 func _process(delta):
 	var velocity = Vector2.ZERO
 	if Input.is_action_pressed("move_right"):
@@ -20,34 +19,23 @@ func _process(delta):
 	if Input.is_action_pressed("move_down"):
 		velocity.y += 1 
 	
-	if velocity.length() > 0:
-		velocity = velocity.normalized() * speed
-		$AnimatedSprite2D.play()
-	else:
-		$AnimatedSprite2D.stop()
+	velocity = velocity.normalized() * speed
+	$AnimatedSprite2D.play()
 	
 	position += velocity * delta
-	position = position.clamp(Vector2.ZERO, screen_size)
+	position = position.clamp(Vector2(), screen_size)
 	
-	if velocity.x == 0 and velocity.y == 0:
+	if (velocity.x == 0 and velocity.y == 0) or velocity.y != 0:
 		$AnimatedSprite2D.animation = "idle"
 	elif velocity.x != 0:
 		$AnimatedSprite2D.animation = "move"
 		$AnimatedSprite2D.flip_v = false
 		#$Trail.rotation = 0
 		$AnimatedSprite2D.flip_h = velocity.x < 0
-	elif velocity.y != 0:
-		$AnimatedSprite2D.animation = "idle"
-		#rotation = PI if velocity.y > 0 else 0 : flips image vertically
+	
 
 func start(pos):
 	position = pos
 	rotation = 0
 	show()
 	$CollisionShape2D.disabled = false
-
-func _on_body_entered(body):
-	hide() #player disappears after being hit
-	hit.emit()
-	$CollisionShape2D.set_deferred("disabled", true)
-	
