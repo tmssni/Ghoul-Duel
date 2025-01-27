977345e2f7a72fd7f610505eaa7d84e9255d8fcd
lavanya-mahajan <lmahajan1971@gmail.com>
2025-01-26 19:46:45 -0500

    more changes

--- ghoul-duel/player.gd
+++ ghoul-duel/player.gd
@@ -10,13 +10,13 @@ func _ready():
 
 func _process(delta):
 	var velocity = Vector2.ZERO
-	if Input.is_action_pressed("move_right"):
+	if Input.is_action_pressed("ui_right"):
 		velocity.x += 1 
-	if Input.is_action_pressed("move_left"):
+	if Input.is_action_pressed("ui_left"):
 		velocity.x -= 1
-	if Input.is_action_pressed("move_up"):
+	if Input.is_action_pressed("ui_up"):
 		velocity.y -= 1  
-	if Input.is_action_pressed("move_down"):
+	if Input.is_action_pressed("ui_down"):
 		velocity.y += 1 
 	
 	velocity = velocity.normalized() * speed
