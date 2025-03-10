0b751d3f6403d554e6bf390b2e2114d3587fd3df
Tira M <146383949+tmssni@users.noreply.github.com>
2025-03-10 14:28:13 -0700

    Update coins.gd

--- ghoul-duel/coins.gd
+++ ghoul-duel/coins.gd
@@ -2,7 +2,7 @@ extends Area2D
 
 var collider_name
 var collider_obj
-
+#class_name coins
 @export var value: int = 1
 @onready var timer: Timer = $Timer
 @onready var area: Area2D #$Area2D
@@ -13,18 +13,21 @@ func _ready():
 func _on_body_entered(body: Node2D) -> void:
 	#if body.name == "purple" or body.name == "green":
 	body.flames_stolen += 1
-	print(body.flames_stolen)
+	
 
 	if body is CharacterBody2D:
 		#GameController.coin_collected(value)
-		#collider_obj = body.get_last_slide_collision()
-		#collider_name = collider_obj.name
+		collider_obj = body.get_collider()
+		collider_name = collider_obj.name
 		#timer.start()
-		#collider_name.visible = false
+		collider_name.visible = false
 		#area.set_deferred("monitoring", false)
-		print("collided with: ", collider_obj.get_path())
+		#body.visible = false
+		print(body.flames_stolen)
+		#print("collided with: ", collider_obj.get_path())
 
 func _on_timeout() -> void:
 	#$Coins.set_deferred("monitoring", true) #"monitoring"
 	collider_name.visible = true
+	#collider_name.visible = true
 	
