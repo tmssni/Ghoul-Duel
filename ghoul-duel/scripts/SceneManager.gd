efdd40cfbcdb8e118d65e970e02081ed9efb6f68
Tira M <146383949+tmssni@users.noreply.github.com>
2025-04-05 18:59:16 -0700

    Update SceneManager.gd

--- ghoul-duel/scripts/SceneManager.gd
+++ ghoul-duel/scripts/SceneManager.gd
@@ -7,22 +7,24 @@ extends Node
 func _ready() -> void:
 	var index = 0 
 	for i in global.GreenPlayers:
-		print("green check")
 		var currentPlayer = GreenPlayerScene.instantiate()
 		PurplePlayerScene.instantiate()
 		add_child(currentPlayer)
+		print(currentPlayer.get_unique_id())
 		for spawn in get_tree().get_nodes_in_group("GreenSpawnPoint"):
 			if spawn.name == str(1):
 				currentPlayer.global_position = spawn.global_position
+				
 		
 	for i in global.PurplePlayers:
-		print("purple check")
 		var currentPlayer = PurplePlayerScene.instantiate()
 		GreenPlayerScene.instantiate()
 		add_child(currentPlayer)
+		print(currentPlayer.get_unique_id())
 		for spawn in get_tree().get_nodes_in_group("PurpleSpawnPoint"):
 			if spawn.name == str(0):
 				currentPlayer.global_position = spawn.global_position
+				
 		
 	pass # Replace with function body.
 
